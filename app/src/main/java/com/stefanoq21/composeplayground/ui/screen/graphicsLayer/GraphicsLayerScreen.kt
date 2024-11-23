0bc46e2f84de41ec169b4e2af54ba594f0c960ce
DESKTOP-P3D71NI\Stefano <stefanoq21@hotmail.it>
2024-11-23 16:23:34 +0100

    GraphicsLayerScreen refined

--- app/src/main/java/com/stefanoq21/composeplayground/ui/screen/graphicsLayer/GraphicsLayerScreen.kt
+++ app/src/main/java/com/stefanoq21/composeplayground/ui/screen/graphicsLayer/GraphicsLayerScreen.kt
@@ -4,22 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
@@ -28,8 +39,9 @@ import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.layer.drawLayer
 import androidx.compose.ui.graphics.rememberGraphicsLayer
 import androidx.compose.ui.layout.ContentScale
@@ -47,6 +59,7 @@ import java.io.FileOutputStream
 
 /** https://developer.android.com/develop/ui/compose/graphics/draw/modifiers#graphics-modifiers    */
 
+
 @Composable
 fun GraphicsLayerScreen(
 
@@ -55,13 +68,127 @@ fun GraphicsLayerScreen(
     val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
 
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
             .padding(16.dp)
     ) {
+
+        var scaleX by remember {
+            mutableFloatStateOf(1f)
+        }
+        TransformationSlider(
+            label = "scaleX", value = scaleX, onValueChange = {
+                scaleX = it
+            }, valueRange = 0f..2f
+        )
+
+        var scaleY by remember {
+            mutableFloatStateOf(1f)
+        }
+        TransformationSlider(
+            label = "scaleY", value = scaleY, onValueChange = {
+                scaleY = it
+            }, valueRange = 0f..2f
+        )
+
+        var translateX by remember {
+            mutableFloatStateOf(0f)
+        }
+        TransformationSlider(
+            label = "translateX",
+            value = translateX,
+            onValueChange = {
+                translateX = it
+            },
+        )
+
+        var translateY by remember {
+            mutableFloatStateOf(0f)
+        }
+        TransformationSlider(
+            label = "translateY",
+            value = translateY,
+            onValueChange = {
+                translateY = it
+            },
+        )
+
+        var transformOrigin by remember {
+            mutableFloatStateOf(0f)
+        }
+        TransformationSlider(
+            label = "transformOrigin",
+            value = transformOrigin,
+            onValueChange = {
+                transformOrigin = it
+            },
+        )
+
+
+        var rotationX by remember {
+            mutableFloatStateOf(0f)
+        }
+        TransformationSlider(
+            label = "rotationX", value = rotationX, onValueChange = {
+                rotationX = it
+            }, valueRange = 0f..360f
+        )
+
+        var rotationY by remember {
+            mutableFloatStateOf(0f)
+        }
+        TransformationSlider(
+            label = "rotationY", value = rotationY, onValueChange = {
+                rotationY = it
+            }, valueRange = 0f..360f
+        )
+
+        var rotationZ by remember {
+            mutableFloatStateOf(0f)
+        }
+        TransformationSlider(
+            label = "rotationZ", value = rotationZ, onValueChange = {
+                rotationZ = it
+            }, valueRange = 0f..360f
+        )
+        var alpha by remember {
+            mutableFloatStateOf(1f)
+        }
+        TransformationSlider(
+            label = "alpha", value = alpha,
+            onValueChange = {
+                alpha = it
+            },
+        )
+
+        var clip by remember {
+            mutableStateOf(false)
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = {
+                clip = !clip
+            }) {
+                Text("Clip")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    val bitmap = graphicsLayer
+                        .toImageBitmap()
+                        .asAndroidBitmap()
+                    shareBitmap(bitmap, context)
+                }
+            }) {
+                Text("Save")
+            }
+        }
+
         Box(modifier = Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Black)
             .drawWithContent {
                 graphicsLayer.record {
                     this@drawWithContent.drawContent()
@@ -70,19 +197,55 @@ fun GraphicsLayerScreen(
             }
             .clickable {
                 coroutineScope.launch {
-                    val bitmap = graphicsLayer.toImageBitmap().asAndroidBitmap()
+                    val bitmap = graphicsLayer
+                        .toImageBitmap()
+                        .asAndroidBitmap()
                     shareBitmap(bitmap, context)
                 }
             }
 
         ) {
-            Text(
-                modifier = Modifier,
-                text = "Graphics Layer",
-                style = MaterialTheme.typography.titleLarge
-            )
+            Surface {
+                Image(
+                    painter = painterResource(id = R.drawable.test_background_1),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                        .graphicsLayer {
+                            this.scaleX = scaleX
+                            this.scaleY = scaleY
+                            this.translationX = (100 * translateX).dp.toPx()
+                            this.translationY = (100 * translateY).dp.toPx()
+                            this.transformOrigin = TransformOrigin(transformOrigin, transformOrigin)
+                            this.rotationX = rotationX
+                            this.rotationY = rotationY
+                            this.rotationZ = rotationZ
+                            this.alpha = alpha
+                            this.clip = clip
+                            this.ambientShadowColor = Color.Black
+                            this.spotShadowColor = Color.Black
+                            this.shape = CircleShape
+                        },
+                    contentScale = ContentScale.Fit
+                )
+
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .blendMode(BlendMode.Difference),
+                    text = "Graphics Layer",
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.displayLarge
+                )
+
+            }
+
         }
 
+
+        Spacer(Modifier.height(16.dp))
 
         val images = remember {
             mutableStateListOf(
@@ -99,10 +262,7 @@ fun GraphicsLayerScreen(
             modifier = Modifier.fillMaxWidth()
 
         ) {
-            val pagerState = rememberPagerState(
-                initialPage = 0,
-                pageCount = { images.size }
-            )
+            val pagerState = rememberPagerState(initialPage = 0, pageCount = { images.size })
             HorizontalPager(
                 state = pagerState
             ) { index ->
@@ -128,16 +288,11 @@ fun GraphicsLayerScreen(
             modifier = Modifier.fillMaxWidth()
 
         ) {
-            val pagerState = rememberPagerState(
-                initialPage = 0,
-                pageCount = { images.size }
-            )
+            val pagerState = rememberPagerState(initialPage = 0, pageCount = { images.size })
             HorizontalPager(
-                modifier = Modifier
-                    .colorFilter(ColorFilter.colorMatrix(ColorMatrix().apply {
-                        setToSaturation(0f)
-                    })),
-                state = pagerState
+                modifier = Modifier.colorFilter(ColorFilter.colorMatrix(ColorMatrix().apply {
+                    setToSaturation(0f)
+                })), state = pagerState
             ) { index ->
                 Image(
                     modifier = Modifier.fillMaxWidth(),
@@ -162,6 +317,23 @@ fun GraphicsLayerScreen(
     }
 
 }
+
+@Composable
+fun TransformationSlider(
+    label: String,
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    valueRange: ClosedFloatingPointRange<Float> = 0f..1f
+) {
+
+    Column {
+        Text(text = label)
+        Slider(
+            value = value, onValueChange = onValueChange, valueRange = valueRange
+        )
+    }
+}
+
 
 private fun Modifier.blendMode(blendMode: BlendMode): Modifier {
     return this.drawWithCache {
@@ -201,23 +373,19 @@ fun shareBitmap(bitmap: Bitmap, context: Context) {
     fos.close()
 
     val uri = FileProvider.getUriForFile(
-        context,
-        "${context.applicationContext.packageName}.fileprovider",
-        imageFile
+        context, "${context.applicationContext.packageName}.fileprovider", imageFile
     )
 
     val shareIntent = Intent(Intent.ACTION_SEND)
     shareIntent.type = "image/jpeg"
     shareIntent.putExtra(
-        Intent.EXTRA_STREAM,
-        uri
+        Intent.EXTRA_STREAM, uri
     )
     shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
 
     context.startActivity(
         Intent.createChooser(
-            shareIntent,
-            "Share Image"
+            shareIntent, "Share Image"
         )
     )
 }
